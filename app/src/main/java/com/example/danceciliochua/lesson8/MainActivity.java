46ee7ea71617144f08112e29898a2865365a1574
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-18 11:17:31 +0800

    Add Project

---
+++ app/src/main/java/com/example/danceciliochua/lesson8/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.danceciliochua.lesson8;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
